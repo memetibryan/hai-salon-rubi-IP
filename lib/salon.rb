6165e19b4fb9f82e7ad41c3946a4492dfeb67039
memetibryan <memetibryan@gmail.com>
2017-07-14 08:57:35 +0300

    added method to delete a salonist entry with all the clients from the database

--- lib/salon.rb
+++ lib/salon.rb
@@ -52,4 +52,9 @@ class Salon
       @id = self.id()
       DB.exec("UPDATE salons SET name = '#{@name}' WHERE id = #{@id};")
     end
+
+    define_method(:delete) do
+      DB.exec("DELETE FROM salons WHERE id = #{self.id()};")
+      DB.exec("DELETE FROM clients WHERE salon_id = #{self.id()};")
+    end
 end
