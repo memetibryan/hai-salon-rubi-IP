0d766f139c9684f94573f877532a41db66f9652b
memetibryan <memetibryan@gmail.com>
2017-07-14 08:30:00 +0300

    initialized the attributes for client

--- lib/client.rb
+++ lib/client.rb
@@ -1,3 +1,8 @@
 class Client
 	attr_reader(:description, :salon_id)
+
+	define_method(:initialize) do |attributes|
+      @description = attributes.fetch(:description)
+      @salon_id = attributes.fetch(:salon_id)
+    end
 end
