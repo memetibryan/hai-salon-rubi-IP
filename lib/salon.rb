79bbe86f9a2091c69925733a561b43e927177785
memetibryan <memetibryan@gmail.com>
2017-07-14 08:38:32 +0300

    added a method functionality to retrieve all database content for the salonist

--- lib/salon.rb
+++ lib/salon.rb
@@ -5,4 +5,15 @@ class Salon
       @name = attributes.fetch(:name)
       @id = attributes.fetch(:id)
     end
+
+    define_singleton_method(:all) do
+      returned_salons = DB.exec("SELECT * FROM salons;")
+      salons = []
+      returned_salons.each() do |salon|
+        name = salon.fetch("name")
+        id = salon.fetch("id").to_i()
+        salons.push(Salon.new({:name => name, :id => id}))
+      end
+      salons
+    end
 end
