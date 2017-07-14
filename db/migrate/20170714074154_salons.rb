f1dc72af3248c24345ced4071fe827fe6d9e258d
memetibryan <memetibryan@gmail.com>
2017-07-14 11:00:33 +0300

    created clients table using rake commmand

--- db/migrate/20170714074154_salons.rb
+++ db/migrate/20170714074154_salons.rb
@@ -1,4 +1,9 @@
 class Salons < ActiveRecord::Migration[5.1]
   def change
+  	create_table(:salons) do |s|
+        s.column(:name, :string)
+
+        s.timestamps()
+    end
   end
 end
---
+++ db/migrate/20170714075925_clients.rb
@@ -0,0 +1,10 @@
+class Clients < ActiveRecord::Migration[5.1]
+  def change
+  	create_table(:clients) do |t|
+        t.column(:name, :string)
+        t.column(:salon_id, :integer)
+
+        t.timestamps()
+    end
+  end
+end
