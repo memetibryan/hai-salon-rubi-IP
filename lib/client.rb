f65d92e552e03777220103378e36a17719d9fc43
memetibryan <memetibryan@gmail.com>
2017-07-14 08:30:56 +0300

    defined a method to fetch all clients in the database

--- lib/client.rb
+++ lib/client.rb
@@ -5,4 +5,15 @@ class Client
       @description = attributes.fetch(:description)
       @salon_id = attributes.fetch(:salon_id)
     end
+
+    define_singleton_method(:all) do
+      returned_clients = DB.exec("SELECT * FROM clients;")
+      clients = []
+      returned_clients.each() do |client|
+        description = client.fetch("description")
+        salon_id = client.fetch("salon_id").to_i() # The information comes out of the database as a string.
+        clients.push(Client.new({:description => description, :salon_id => salon_id}))
+      end
+      clients
+    end
 end
