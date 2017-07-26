bfd011861fe716047972b9b67f24faa5ff2db64b
memetibryan <memetibryan@gmail.com>
2017-07-26 08:31:38 +0300

    removed .codeclimate file

--- app.rb
+++ app.rb
@@ -70,27 +70,3 @@ require("bundler/setup")
     @salons = Salon.all()
     erb(:salon_success)
   end
-
-  # redirect code
-  # post ('/clients')
-  #   @new_client = Client.new(params.fetch("namme"))
-  #   if @new_client.save()
-  #     redirect("/clients/".concat(@new_client.id().to_s()))
-  #   else
-  #     erb(:index)
-  #   end
-  # end
-
-  # get('/clients/:id') do
-  #   @client = Client.find(params.fetch("id").to_i())
-  #   erb(:client)
-  # # end
-
-  # delete('/tasks/:id') do
-  #   @task = Task.find(params.fetch("id").to_i()
-  #   if @task.destroy()
-  #     redirect("/tasks")
-  #   else
-  #     erb(:task)
-  #   end
-  # end
